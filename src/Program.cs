cf9ae65e7804450eb6f866a49d2577d7bd7e7fa8
Maximilian <Maximilian0712@outlook.de>
2024-09-16 15:25:18 +0200

    Add DatabaseOptions configuration and register MyMasternodeSetupContext as singleton

--- src/Program.cs
+++ src/Program.cs
@@ -1,8 +1,22 @@
+using Blvckout.MyMasternode.Setup.Options;
+using Blvckout.MyMasternode.Setup.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Read environment variables
+builder.Configuration.AddEnvironmentVariables();
+
+// Check configuration
+builder.Services.AddOptions<DatabaseOptions>()
+    .Bind(builder.Configuration.GetSection(DatabaseOptions.SECTION))
+    .ValidateDataAnnotations()
+    .ValidateOnStart();
+
 // Add services to the container.
+builder.Services.AddSingleton<MyMasternodeSetupContext>();
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
